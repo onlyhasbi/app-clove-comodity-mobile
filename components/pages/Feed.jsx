70f27f3d0d74fac92672571eb856af7c83fa2f5f
sofyan ardiansyah <sofyanardianysah@gmail.com>
2023-06-05 10:16:07 +0800

    feed change

--- components/pages/Feed.jsx
+++ components/pages/Feed.jsx
@@ -26,7 +26,7 @@ const FeedCard = ({ title, type, text, author, authorAvatar, image }) => (
     <div className="px-4 py-4 bg-white rounded-b-xl dark:bg-gray-900">
       <h4 className="font-bold py-0 text-s text-gray-400 dark:text-gray-500 uppercase">{type}</h4>
       <h2 className="font-bold text-2xl text-gray-800 dark:text-gray-100">{title}</h2>
-      <p className="sm:text-sm text-s text-gray-500 mr-1 my-3 dark:text-gray-400">{text}</p>
+      <p className="sm:text-sm text-s text-gray-500 mr-1 my-3 dark:text-gray-400">{text} test</p>
       <div className="flex items-center space-x-4">
         <div className="w-10 h-10 relative">
           <img src={authorAvatar} className="rounded-full object-cover min-w-full min-h-full max-w-full max-h-full" alt="" />
